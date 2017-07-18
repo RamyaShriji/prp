5b174c3e3796e16c2901cd85cd3ef3828ea39473
RamyaShriji <ramyashriji.97@gmail.com>
2017-07-18 15:13:24 +0530

    Create smaller.java

---
+++ smaller.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class smaller 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+        System.out.println("ENTER THE ARRAY LENGTH");
+		int n=s.nextInt();
+		int[] a=new int[n];
+		System.out.println("ENTER THE ARRAY VALUES");
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<n-1;i++)
+		{
+			if(a[i]>a[i+1])
+			{
+				System.out.print(a[i+1]+" ");
+			}
+			if(a[i]<a[i+1])
+			{
+				System.out.print("-1");
+			}
+		}
+
+	}
+
+}
+
