763a04027f47b7271b2bc26297011201bda5780c
RamyaShriji <ramyashriji.97@gmail.com>
2017-07-20 18:18:33 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class reverse
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String s1=s.next();
+        String s2="";
+        StringBuffer sb=new StringBuffer(s1);
+        s2=sb.reverse().toString();
+        System.out.print(s2);
+        
+        
+        
+    }
+}
+
